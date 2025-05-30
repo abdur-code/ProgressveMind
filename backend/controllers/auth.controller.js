bcee509e9a1ac686133b97d837dea57a77de150b
Abdur Rahman <abdurrahmanim2422@gmail.com>
2025-05-30 10:45:32 +0530

    Added middleware and function to handle errors

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res, next) => {
+    const {username, email, password} = req.body;
+
+    if(!username || !email || !password || email.trim() === '' || password.trim() === '' || username.trim() === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    const existingUser = await User.findOne({email});
+    if(existingUser) {
+        next(errorHandler(400, 'User already exists'));
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    
+    try {
+        await newUser.save();
+        res.json({
+            message: 'User created successfully',
+        });
+    } catch (error) {
+        next(error);
+    }
+}
