640d6b0ad723beacd8b9893c4d9eaeb10226d962
Abdur Rahman <abdurrahmanim2422@gmail.com>
2025-05-30 21:08:34 +0530

    sign in api route completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import errorHandler from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -42,17 +43,17 @@ export const signin = async (req, res, next) => {
     try {
         const validUser = await User.findOne({email});
         if(!validUser) {
-            next(errorHandler(400, 'User not found'));
+            return next(errorHandler(400, 'User not found'));
         }
 
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) {
-            next(errorHandler(400, 'Invalid credentials'));
+             return next(errorHandler(400, 'Invalid credentials'));
         }
 
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         res.status(200).cookie('access_token', token, {httpOnly: true}).json({
-            message: 'User signed in successfully',
+            validUser
         });
     } catch (error) {
         next(error);
